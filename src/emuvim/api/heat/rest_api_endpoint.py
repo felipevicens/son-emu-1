f666ba7eec9b49f00d74c6b9fe6b72d03ba5b205
Georg Stilow <georg+github@stilow.de>
2016-07-25 14:17:46 +0200

    First heat api outline

---
+++ src/emuvim/api/heat/rest_api_endpoint.py
@@ -0,0 +1,36 @@
+
+
+import logging
+import compute
+import network
+import YAMLReader
+
+class RestApiEndpoint(object):
+
+    def __init__(self, listenip, port):
+        self.ip = listenip
+        self.port = port
+
+    def connectDatacenter(self, dc):
+        compute.dcs[dc.label] = dc
+        logging.info(
+            "Connected DC(%s) to API endpoint %s(%s:%d)" % (dc.label, self.__class__.__name__, self.ip, self.port))
+
+
+    def connectDCNetwork(self, DCnetwork):
+
+        network.net = DCnetwork
+        # monitor.net = DCnetwork # TODO add the monitor part
+
+        logging.info("Connected DCNetwork to API endpoint %s(%s:%d)" % (
+            self.__class__.__name__, self.ip, self.port))
+
+    def start(self):
+        # TODO Start a thread for the REST API listener
+        self.read_heat_file()
+
+    def read_heat_file(self):
+        inputFile = open('yamlTest1', 'r')
+        inp = inputFile.read()
+        reader = YAMLReader.YAMLReader()
+        reader.parse_input(inp)
