e19cbe0e5d75bf8d08efa1dbf1fed932daae042a
Georg Stilow <georg+github@stilow.de>
2016-08-01 16:27:57 +0200

    Update api structure
    Renamed yaml_reader to heat_parser.
    Created api classes to store all configurations.

--- src/emuvim/api/heat/compute.py
+++ src/emuvim/api/heat/compute.py
@@ -1,36 +1,10 @@
+from resources import *
 
-import yaml
 
-dcs = {}
+class HeatCompute:
+    def __init__(self):
+        self.dcs = dict()
+        self.stacks = dict()
 
-server_list = []
-
-def add_server(compute_name, network, image, command):
-    server = Heat_Server(compute_name, network, image, command)
-    server_list.append(server)
-
-def parse_server_network(network):
-    '''
-    TODO how to create networks like
-     [{"id":x, "ip":"x.x.x.x/x"}, ...]
-    from our example?
-    '''
-    newNetwork = {}
-    iterator = 0
-    for port in network: #TODO change the IPs ! the example does not contain real IPs
-        newNetwork.update({'id': iterator, 'ip': port['port']['get_resource']}) #it rewrites the id and ip each time! thus we only get one entry but we want to have all 3
-        iterator += 1
-    #print(yaml.dump(newNetwork, default_flow_style=False))
-    return newNetwork
-
-class Heat_Server:
-
-    def __init__(self, compute_name, network, image, command):
-        self.compute_name = compute_name
-        self.network = network
-        self.image = image
-        self.command = command
-
-    def start_compute(self, dc_label):
-        c = dcs.get(dc_label).startCompute(self.compute_name, self.image, self.command, self.network)
-        return c.getStatus()
+    def add_stack(self, stack):
+        self.stacks[stack.id] = stack
---
+++ src/emuvim/api/heat/resources/instance_flavor.py
@@ -0,0 +1,8 @@
+
+class InterfaceFlavor:
+    def __init__(self, cpu=None, memory=None, memory_unit=None, storage=None, storage_unit=None):
+        self.cpu = cpu
+        self.memory = memory
+        self.memory_unit = memory_unit
+        self.storage = storage
+        self.storage_unit = storage_unit
