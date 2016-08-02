98a8eae041c4000a182db0c961855a554e4b41d3
joka <joka@mail.uni-paderborn.de>
2016-08-02 11:26:11 +0200

    Just work with a single datacenter for now
    Since we need a RestAPIEndpoint at each DC we don't need to keep track of more than 1 DC. Also extend the server definition to be able to read images etc.

--- src/emuvim/api/heat/network.py
+++ src/emuvim/api/heat/network.py
@@ -1,4 +1,24 @@
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+
 
 class HeatNet:
     def __init__(self):
         self.net = None
+
+    def network_action_start(self, vnf_src_name, vnf_dst_name, **kwargs):
+        try:
+            c = self.net.setChain(
+                vnf_src_name, vnf_dst_name,
+                vnf_src_interface=kwargs.get('vnf_src_interface'),
+                vnf_dst_interface=kwargs.get('vnf_dst_interface'),
+                cmd='add-flow',
+                weight=kwargs.get('weight'),
+                match=kwargs.get('match'),
+                bidirectional=kwargs.get('bidirectional'),
+                cookie=kwargs.get('cookie'))
+            return str(c)
+        except Exception as ex:
+            logging.exception("RPC error.")
+            return ex.message
