2215b07e4d76dea3c91c029561b05a8d41e4715f
Georg Stilow <georg+github@stilow.de>
2016-12-05 16:09:02 +0100

    Server should be found via name too

--- src/emuvim/api/heat/compute.py
+++ src/emuvim/api/heat/compute.py
@@ -117,7 +117,7 @@ class OpenstackCompute(object):
         # Stop all servers and their links of this stack
         for server in self.stacks[stack_id].servers.values():
             self._stop_compute(server)
-            self.delete_server(server.id)
+            self.delete_server(server)
         for net in self.stacks[stack_id].nets.values():
             self.delete_network(net.id)
         for port in self.stacks[stack_id].ports.values():
@@ -271,14 +271,14 @@ class OpenstackCompute(object):
                 self._remove_link(server.name, link)
 
         self.dc.stopCompute(server.name)
-        self.delete_server(server.id)
+        self.delete_server(server)
 
     def find_server_by_name_or_id(self, name_or_id):
         if name_or_id in self.computeUnits:
             return self.computeUnits[name_or_id]
 
         for server in self.computeUnits.values():
-            if server.id == name_or_id or server.template_name == name_or_id or server.full_name == name_or_id:
+            if server.name == name_or_id or server.template_name == name_or_id or server.full_name == name_or_id:
                 return server
         return None
 
@@ -290,8 +290,7 @@ class OpenstackCompute(object):
         self.computeUnits[server.id] = server
         return server
 
-    def delete_server(self, name_or_id):
-        server = self.find_server_by_name_or_id(name_or_id)
+    def delete_server(self, server):
         if server is None:
             return False
 
